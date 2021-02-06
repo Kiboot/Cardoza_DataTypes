cf45b05a212dae137b4c1adcf5f0b37313770d22
Kiboot <kevinlee.cardoza@gmail.com>
2021-02-06 18:19:25 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/cardoza_datatypes/MainActivity.java
@@ -0,0 +1,34 @@
+package mcm.edu.ph.cardoza_datatypes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView display;
+        display = findViewById(R.id.displayTxt);
+
+        String firstName = "Kevin Lee";
+        char middleInitial ='D';
+        String lastname = "Cardoza";
+        int myAge = 29;
+        float myDreamTVLGrade = 95.0f;
+        double myMoney =99999.99;
+
+        //String.valueOf(myMoney);
+        display.setText("My name is "+firstName+ " "
+                +middleInitial+ " "
+                +lastname+ "\nand my age is "
+                +myAge+ ".\nI want my grade in TVL3 to be "
+                +String.valueOf(myDreamTVLGrade));
+
+        boolean amIMarried = false;
+    }
+}
